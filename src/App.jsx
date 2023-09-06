13839ff7096c3d28f6b1eb87c49bba0619af4eec
GuravPratik <pratikgurav1010@gmail.com>
2023-09-06 13:52:15 +0530

    delete education section

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import Navbar from "./components/Navbar";
 import About from "./components/About";
 import Skills from "./components/Skills";
-
 import Project from "./components/Project";
 
 export default function App() {
